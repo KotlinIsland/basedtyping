bbc0a21d7aecde471c61c948dd8946a9668758c0
DetachHead <detachhead@users.noreply.github.com>
2021-12-31 00:08:21 +1000

    remove duplicate code

--- basedtyping/reified_generic.py
+++ basedtyping/reified_generic.py
@@ -14,16 +14,11 @@ class _ReifiedGenericAlias(_GenericAlias, _root=True):
         basically you should always use this instead of ``self.__parameters__``"""
         return cast(tuple[TypeVar, ...], getattr(self.__origin__, "__parameters__"))
 
-    def __subclasscheck__(self, subclass: object) -> bool:
-        # could be any random class, check it first
-        if not isinstance(subclass, _GenericAlias) or not is_subclass(
-            subclass.__origin__, self.__origin__
-        ):
-            return False
+    def _type_var_check(self, args: tuple[type, ...]) -> bool:
         for parameter, self_arg, subclass_arg in zip(
             self._type_vars(),
             self.__args__,
-            subclass.__args__,
+            args,
             strict=True,
         ):
             if parameter.__covariant__ and not is_subclass(subclass_arg, self_arg):
@@ -34,25 +29,21 @@ class _ReifiedGenericAlias(_GenericAlias, _root=True):
                 return False
         return True
 
+    def __subclasscheck__(self, subclass: object) -> bool:
+        # could be any random class, check it first
+        if not isinstance(subclass, _GenericAlias) or not is_subclass(
+            subclass.__origin__, self.__origin__
+        ):
+            return False
+        return self._type_var_check(subclass.__args__)
+
     def __instancecheck__(self, instance: object) -> bool:
         # could be any random instance, check it first
         if not isinstance(instance, self.__origin__) or not isinstance(
             instance, ReifiedGeneric
         ):
             return False
-        for parameter, self_arg, subclass_arg in zip(
-            self._type_vars(),
-            self.__args__,
-            instance.__orig_class__.__args__,
-            strict=True,
-        ):
-            if parameter.__covariant__ and not is_subclass(subclass_arg, self_arg):
-                return False
-            if parameter.__contravariant__ and not is_subclass(self_arg, subclass_arg):
-                return False
-            if self_arg is not subclass_arg:
-                return False
-        return True
+        return self._type_var_check(instance.__orig_class__.__args__)
 
 
 class OrigClass(Protocol):
