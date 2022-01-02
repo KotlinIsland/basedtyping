c8c387cbab3d27c43ea78952eb1abdb8aecb25f1
DetachHead <detachhead@users.noreply.github.com>
2022-01-03 00:30:34 +1000

    add more tests for `Function` type based on the `types` module

--- tests/test_callables.py
+++ tests/test_callables.py
@@ -5,5 +5,33 @@ from basedtyping.type_checks import assert_type
 
 if TYPE_CHECKING:
     # these are just type-time tests, not real life pytest tests. they are only run by mypy
+
+    assert_function = assert_type[Function]
+
+    def test_lambda_type() -> None:
+        assert_function(lambda: ...)
+
     def test_function_type() -> None:
-        assert_type[Function](lambda: ...)
+        def func() -> None:
+            ...
+
+        assert_function(func)
+
+    def test_builtin_function() -> None:
+        assert_function(len)
+
+    def test_builtin_method() -> None:
+        assert_function([].append)
+
+    def test_wrapper_descriptor() -> None:
+        assert_function(object.__init__)
+
+    def test_method_wrapper() -> None:
+        assert_function(object().__str__)
+
+    def test_method_descriptor() -> None:
+        assert_function(str.join)
+
+    def test_class_method_descriptor() -> None:
+        # method signature contains `Any`
+        assert_function(dict.fromkeys)  # type:ignore[misc]
