ad0d49820e7d1808263125faf8dfb57f4363f724
DetachHead <detachhead@users.noreply.github.com>
2022-01-04 16:24:50 +1000

    add test for instanciating `ReifiedGeneric` with not enough type vars

--- tests/test_reified_generics/test_not_reified/test_bound.py
+++ tests/test_reified_generics/test_not_reified/test_bound.py
@@ -1,4 +1,6 @@
 """mypy should catch these, but it doesn't due to https://github.com/python/mypy/issues/7084"""
+from typing import TypeVar
+
 from pytest import raises
 
 from basedtyping import MissingTypeParametersError, ReifiedGeneric, T
@@ -20,5 +22,19 @@ def test_isinstance() -> None:
     def function(_value: T) -> None:
         with raises(MissingTypeParametersError):
             isinstance(Reified[str](), Reified[T])  # type:ignore[misc]
+
+    function("")
+
+
+def test_not_enough_type_params() -> None:
+    """mypy does catch this one"""
+    U = TypeVar("U")
+
+    class Reified2(ReifiedGeneric[tuple[T, U]]):
+        pass
+
+    def function(_value: T) -> None:
+        with raises(TypeError):
+            Reified2[int]()  # type:ignore[misc]
 
     function("")
