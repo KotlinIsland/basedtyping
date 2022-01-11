b6cc10addef221f75ec091b41d8572d6a12f9dc1
DetachHead <detachhead@users.noreply.github.com>
2022-01-11 19:16:09 +1000

    rewrite reified generic

--- tests/test_reified_generics/test_reified_generic.py
+++ tests/test_reified_generics/test_reified_generic.py
@@ -1,8 +1,9 @@
+from types import NoneType
 from typing import Generic, TypeVar
 
 from pytest import raises
 
-from basedtyping import NoParametersError, ReifiedGeneric, T
+from basedtyping import NotReifiedError, ReifiedGeneric, T
 
 T2 = TypeVar("T2")
 
@@ -19,33 +20,30 @@ class Normal(Generic[T, T2]):
     pass
 
 
+no_parameters_error_match = "Cannot instantiate ReifiedGeneric "
+not_reified_parameter_error_match = "TypeVars cannot be used"
+
+
 def test_class_args_and_params_class() -> None:
     assert (
         Normal[int, str].__args__  # type: ignore[attr-defined,misc]
-        == Reified[int, str].__args__
+        == Reified[int, str].__reified_generics__
     )
     assert (
-        Normal[int, str].__origin__.__parameters__  # type: ignore[attr-defined,misc]
-        == Reified[int, str].__origin__.__parameters__  # type: ignore[attr-defined,misc]
+        Normal[int, str].__parameters__  # type: ignore[attr-defined,misc]
+        == Reified[int, str].__type_vars__
     )
 
 
 def test_class_args_and_params_instance() -> None:
-    assert (
-        Normal[int, str]().__orig_class__.__args__  # type: ignore[attr-defined,misc]
-        == Reified[int, str]().__orig_class__.__args__
-    )
-    assert (
-        Normal[int, str]().__orig_class__.__origin__.__parameters__  # type: ignore[attr-defined,misc]
-        == Reified[int, str]().__orig_class__.__origin__.__parameters__  # type: ignore[attr-defined,misc]
-    )
+    assert Reified[int, str]().__reified_generics__ == (int, str)
+    assert not Reified[int, str]().__type_vars__
 
 
 def test_reified_list() -> None:
-    it = ReifiedList[int]([1, 2, 3]).__orig_class__
-    assert it.__origin__ == ReifiedList  # type: ignore[attr-defined,misc]
-    assert it.__args__ == (int,)
-    assert it.__parameters__ == ()
+    it = ReifiedList[int]([1, 2, 3])
+    assert it.__reified_generics__ == (int,)
+    assert not it.__type_vars__
 
 
 # https://github.com/KotlinIsland/basedmypy/issues/5
@@ -60,8 +58,8 @@ def test_issubclass() -> None:
 
 
 def test_reified_generic_without_generic_alias() -> None:
-    with raises(NoParametersError):
-        Reified()  # pylint:disable=no-value-for-parameter
+    with raises(NotReifiedError, match=no_parameters_error_match):
+        Reified()
 
 
 def test_subclass() -> None:
@@ -71,10 +69,10 @@ def test_subclass() -> None:
     class SubReified2(Reified[T, T2], ReifiedGeneric[tuple[T, T2]]):
         pass
 
-    with raises(NoParametersError):
+    with raises(NotReifiedError, match=no_parameters_error_match):
         SubReified1()
-    with raises(NoParametersError):
-        SubReified2()  # pylint:disable=no-value-for-parameter
+    with raises(NotReifiedError, match=no_parameters_error_match):
+        SubReified2()
     s = SubReified2[int, str]()
     assert isinstance(s, Reified[int, str])  # type: ignore[misc]
     assert not isinstance(s, Reified[str, int])  # type: ignore[misc]
@@ -89,9 +87,24 @@ def unsupported_concrete_subclass() -> None:
 
 def test_reified_in_init() -> None:
     class Foo(ReifiedGeneric[T]):
-        # https://github.com/PyCQA/pylint/issues/5637
-        def __init__(self) -> None:  # pylint:disable=super-init-not-called
-            assert self.__orig_class__.__args__ == (int,)
+        def __init__(self) -> None:
+            assert self.__reified_generics__ == (int,)
 
     Foo[int]()
     assert not hasattr(Foo, "__orig_class__")
+
+
+def test_concrete_subclass() -> None:
+    class A(ReifiedGeneric[T]):
+        pass
+
+    class SubASpecified(A[int]):
+        pass
+
+    print(SubASpecified.mro())
+    assert issubclass(SubASpecified, A[int])  # type: ignore[misc]
+    assert not issubclass(SubASpecified, A[str])  # type: ignore[misc]
+
+    s = SubASpecified()
+    assert isinstance(s, A[int])  # type: ignore[misc]
+    assert not isinstance(s, A[str])  # type: ignore[misc]
