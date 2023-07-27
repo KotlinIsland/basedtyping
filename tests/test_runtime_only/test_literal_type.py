cd6ad141437c2f6657316ee44219d40430bff84d
detachhead <detachhead@users.noreply.github.com>
2023-07-27 15:09:59 +1000

    fix mypy errors on `LiteralType` tests in <3.9

--- tests/test_runtime_only/test_literal_type.py
+++ tests/test_runtime_only/test_literal_type.py
@@ -3,18 +3,18 @@ from typing import Union
 
 import pytest
 
+if sys.version_info >= (3, 9):  # prevent mypy errors
 
-@pytest.mark.skipif(sys.version_info < (3, 9), reason="need 3.9 for LiteralType")
-def test_literal_type_positive() -> None:
-    from typing import Literal
+    @pytest.mark.skipif(sys.version_info < (3, 9), reason="need 3.9 for LiteralType")
+    def test_literal_type_positive() -> None:
+        from typing import Literal
 
-    from basedtyping.runtime_only import LiteralType
+        from basedtyping.runtime_only import LiteralType
 
-    assert isinstance(Literal[1, 2], LiteralType)
+        assert isinstance(Literal[1, 2], LiteralType)
 
+    @pytest.mark.skipif(sys.version_info < (3, 9), reason="need 3.9 for LiteralType")
+    def test_literal_type_negative() -> None:
+        from basedtyping.runtime_only import LiteralType
 
-@pytest.mark.skipif(sys.version_info < (3, 9), reason="need 3.9 for LiteralType")
-def test_literal_type_negative() -> None:
-    from basedtyping.runtime_only import LiteralType
-
-    assert not isinstance(Union[int, str], LiteralType)
+        assert not isinstance(Union[int, str], LiteralType)
